4abbccdfe3e489946d0007b327b099025d4cda02
eric nubor <nuboreric6@gmail.com>
2024-05-07 20:20:36 +0000

    update @ 7/05/24

--- src/pages/addproject/modal.jsx
+++ src/pages/addproject/modal.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
 const Modal = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <h1>Upload</h1>
+        <div className="form">
+          <h2>Upload Files </h2>
+          <div className="file-upload-container">
+            {/* File input with custom placeholder */}
+            <label
+              className="custom-file-upload"
+              onDrop={handleDrop}
+              onDragOver={(event) => event.preventDefault()}
+            >
+              <input
+                id="file-upload"
+                type="file"
+                accept=".pdf,.docx"
+                onChange={handleFileUpload}
+              />
+              {selectedFile ? selectedFile.name : "Drop Item Here!"}
+              <p>or</p>
+              
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
