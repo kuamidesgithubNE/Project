8b5cf6ea7ad82f73a8f2f938c01eff9ade0abd11
eric nubor <nuboreric6@gmail.com>
2024-05-14 19:21:30 +0000

    update @ 12/05/24

--- src/pages/projects/ProjectsContent.jsx
+++ src/pages/projects/ProjectsContent.jsx
@@ -1,9 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Card from "../../components/card";
+import "./project.css";
+
 const ProjectContent = () => {
-    return ( 
-        <div className="container">
-            <h1>Projects</h1>
-        </div>
-     );
-}
- 
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/data.json");
+      setJsonData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Projects</h1>
+      <div className="grid-container">
+        <Card jsonData={jsonData} />
+      </div>
+    </div>
+  );
+};
+
 export default ProjectContent;
